3e3d070a28aaf3fbeb8ee8509d1a27af7aa95b45
WeisongZhao <zhaoweisong950713@163.com>
2022-05-21 16:48:46 +0800

    nothing

--- src/main/java/com/HIT/weisongzhao/AboutTVJ_.java
+++ src/main/java/com/HIT/weisongzhao/AboutTVJ_.java
@@ -8,20 +8,16 @@ import ij.ImageJ;
 import ij.plugin.PlugIn;
 
 public class AboutTVJ_ extends JDialog implements PlugIn {
-	private String defaultMessage = "(c) 2021 	HIT";
+	private String defaultMessage = "(c) 2022 HIT";
 	private WalkBar_TVJ walk = new WalkBar_TVJ(this.defaultMessage, true, false, true);
 
 	@Override
 	public void run(String arg) {
-		this.walk.fillAbout("t-variance ImageJ plugin", " 06/04/2021", "t axial variance for calcium signal profiling<br>v0.1.3",
-				"School of Instrumentation Science and Engineering<br/>Harbin Institute of Technology",
-				"Weisong Zhao (zhaoweisong950713@163.com)", "2020",
+		this.walk.fillAbout("t-variance ImageJ plugin", "05/21/2022 updated", "t axial variance for calcium signal profiling<br>v0.1.3",
+				"School of Instrumentation Science and Engineering","Harbin Institute of Technology",
+				"Weisong Zhao",
 				"<p style=\"text-align:left\">"
-				+ "<b>Publications:</b>"
-				+ "<br> XXXXXXXXXXXXXXXXXXXXXXX"
-				+ "<br><br><b>References:</b>"
-				+ "<br>[1] D. Sage, L. Donati, F. Soulez, D. Fortun, G. Schmit, A. Seitz, R. Guiet, C. Vonesch, and M. Unser, \"DeconvolutionLab2: An open-source software for deconvolution microscopy,\" Methods vol. 115, no. 28, 2017."				
-				+ "<br><br><b>Acknowledgements:</b><br>This plugin is built on the top of DeconvolutionLab2 [1]"
+				+ "<b>Usage:</b><br>It is a simple FIJI/ImageJ plugin for t-axial variance calculation assisting in calcium signal profiling. It will highlight the regions that have calcium signal transients."
 				+ "<br><br><b>Open source:</b><br>https://github.com/WeisongZhao/tvarianceJ"
 				+ "");
 		
